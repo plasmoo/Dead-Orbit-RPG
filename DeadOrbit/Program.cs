ef6617f3b27498ede92d996133fec7ef200397b0
plasmoo <abarreasprogrammer@gmail.com>
2023-11-10 12:43:40 -0500

    Project setup.

---
+++ DeadOrbit/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DeadOrbit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("First Commit!");
+        }
+    }
+}
