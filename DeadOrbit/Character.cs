e9c8b1b83b5813137b08a1a1929b10e7cd679815
plasmoo <150185074+plasmoo@users.noreply.github.com>
2023-11-21 15:02:58 -0500

    Initial game set up  (#1)
    * Adding development branch.
    
    * Added GameText class to add dialogue into the game. Began work on Character class to create player characters and npc's.
    
    * Added character ability scores, a dice rolling mechanic, and a method for changing the color of hostile npc's names.
    
    * Added new method to change color of Instruction Text. Added character information section in main program.
    
    ---------
    
    Co-authored-by: plasmoo <abarreasprogrammer@gmail.com>

---
+++ DeadOrbit/Character.cs
@@ -0,0 +1,84 @@
+﻿public class Character
+{
+    // Character Info 
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public bool IsMale { get; set; }
+    public string Background { get; set; }
+
+    //Character Stats
+    public int Strength { get; set; }
+    public int Agility { get; set; }
+    public int Perception { get; set; }
+    public int Intelligence { get; set; }
+    public int Charisma { get; set; }
+
+    //Creating instance of Random Class for Dice Roller
+    Random random = new Random();
+
+    //Dice Roller
+    public int DiceRoller(string abilityType, int passingScore)
+    {
+        int AbilityScore = 0;
+
+        switch (abilityType)
+        {
+            case "Strength":
+                AbilityScore = this.Strength;
+                break;
+            case "Agility":
+                AbilityScore = this.Agility;
+                break;
+            case "Perception":
+                AbilityScore = this.Perception;
+                break;
+            case "Intelligence":
+                AbilityScore = this.Intelligence;
+                break;
+            case "Charisma": 
+                AbilityScore = this.Charisma;
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.Red; 
+                Console.WriteLine("WARNING: Invalid ability type input (Remember ability type is case sensitive");
+                Console.ResetColor();
+                return -1; 
+        }
+
+        int DiceRoll = random.Next(1, 21);
+        int RollPlusModifier = DiceRoll + AbilityScore; 
+        Console.WriteLine($"You rolled a {RollPlusModifier}!");
+
+
+        if (RollPlusModifier >= passingScore)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Your {abilityType} succeeded!");
+            Console.ResetColor();
+        } else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Your {abilityType} failed!");
+            Console.ResetColor();
+        }
+
+        return RollPlusModifier;
+    }
+
+
+
+        //Constructor
+        public Character(string name, int age, bool isMale, string background, int strength, int agility, int perception,
+            int intelligence, int charisma)
+        {
+            Name = name;
+            Age = age;
+            IsMale = isMale;
+            Background = background;
+            Strength = strength;
+            Agility = agility;
+            Perception = perception;
+            Intelligence = intelligence;
+            Charisma = charisma;
+        }
+    }
---
+++ DeadOrbit/GameText.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class GameText
+{
+
+	public void FriendlyNPC(string name)
+	{
+		Console.ForegroundColor = ConsoleColor.Green;
+		Console.WriteLine(name);
+		Console.ResetColor();
+	}
+
+    public void HostileNPC(string name)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(name);
+        Console.ResetColor();
+    }
+
+	public void InstructionText(string message)
+	{
+		Console.ForegroundColor = ConsoleColor.Yellow;
+		Console.WriteLine(message);
+		Console.ResetColor();
+	}
+
+
+    public void GameIntro()
+	{
+        Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine("----------------\r\n|  Dead Orbit  |\r\n----------------\r\n");
+		Console.ForegroundColor = ConsoleColor.Yellow;
+		Console.WriteLine("Welcome, Pilot. Dead Orbit is a text-based roleplaying game where the world is narrated to you" +
+            "through text in the console, and you'll be prompted to respond through console commands. It is set " +
+            "in the year 2350 and you are a Deep Space Pilot, a talented individual entrusted to make perilous journeys" +
+			" into 'Deep Space', deeply isolated regions of space subject to a wide variety of threats from " +
+			"blackholes to space marauders. You are currently en-route to the Outer-Rim colony of Europa. Despite " +
+			"your initial bad feelings about this job it has proven to be a quiet trip. You've been hired to " +
+			"transport food and medical supplies to the colony and are in the process of entering the planet's orbit.  ");
+		Console.ResetColor();
+
+		FriendlyNPC("\nSincere Arrington:");
+		Console.WriteLine("This is Sincere Arrington, Head of Security here at Europa. Please identify yourself and " +
+			"forward your credentialsS."); 
+	}
+
+	public void PilotCredentials()
+	{
+		
+	}
+
+
+
+
+	//Constructor
+	public GameText()
+	{
+
+	}
+}
