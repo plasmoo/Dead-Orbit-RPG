9838985d6a503a1cec05d3f5fc90330e4a63e05c
plasmoo <150185074+plasmoo@users.noreply.github.com>
2023-11-22 21:14:55 -0500

    Input Verfication for player credentials  (#2)
    * Adding development branch.
    
    * Added GameText class to add dialogue into the game. Began work on Character class to create player characters and npc's.
    
    * Added character ability scores, a dice rolling mechanic, and a method for changing the color of hostile npc's names.
    
    * Added new method to change color of Instruction Text. Added character information section in main program.
    
    * Changed HostileNPC and FriendlyNPC method names to the more generic GreenText and RedText. Also added input verification for gender input.
    
    * Added input verification for background selection.
    
    * Renamed InstructionText method to the more generic YellowText method.
    
    * Added input verification for age selection.
    
    * Added player input for ability scores and input verification.
    
    ---------
    
    Co-authored-by: plasmoo <abarreasprogrammer@gmail.com>

--- DeadOrbit/GameText.cs
+++ DeadOrbit/GameText.cs
@@ -3,21 +3,21 @@
 public class GameText
 {
 
-	public void FriendlyNPC(string name)
+	public void GreenText(string name)
 	{
 		Console.ForegroundColor = ConsoleColor.Green;
 		Console.WriteLine(name);
 		Console.ResetColor();
 	}
 
-    public void HostileNPC(string name)
+    public void RedText(string name)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine(name);
         Console.ResetColor();
     }
 
-	public void InstructionText(string message)
+	public void YellowText(string message)
 	{
 		Console.ForegroundColor = ConsoleColor.Yellow;
 		Console.WriteLine(message);
@@ -27,10 +27,8 @@ public class GameText
 
     public void GameIntro()
 	{
-        Console.ForegroundColor = ConsoleColor.Red;
-		Console.WriteLine("----------------\r\n|  Dead Orbit  |\r\n----------------\r\n");
-		Console.ForegroundColor = ConsoleColor.Yellow;
-		Console.WriteLine("Welcome, Pilot. Dead Orbit is a text-based roleplaying game where the world is narrated to you" +
+		RedText("----------------\r\n|  Dead Orbit  |\r\n----------------\r\n");
+		YellowText("Welcome, Pilot. Dead Orbit is a text-based roleplaying game where the world is narrated to you" +
             "through text in the console, and you'll be prompted to respond through console commands. It is set " +
             "in the year 2350 and you are a Deep Space Pilot, a talented individual entrusted to make perilous journeys" +
 			" into 'Deep Space', deeply isolated regions of space subject to a wide variety of threats from " +
@@ -39,13 +37,25 @@ public class GameText
 			"transport food and medical supplies to the colony and are in the process of entering the planet's orbit.  ");
 		Console.ResetColor();
 
-		FriendlyNPC("\nSincere Arrington:");
+		GreenText("\nSincere Arrington:");
 		Console.WriteLine("This is Sincere Arrington, Head of Security here at Europa. Please identify yourself and " +
 			"forward your credentialsS."); 
 	}
 
-	public void PilotCredentials()
+	public void AbilityCheckInfo()
 	{
+        YellowText("Dead Orbit is a dice based roleplaying game, meaning that whether or not you " +
+                "succeed in an action is determined by the roll of a D20 (a twenty sided die), adding on " +
+                "your ability modifiers. There are 5 Ability Modifiers: Strength, Agility, Perception " +
+                "Intelligence, and Charisma. \n [Strength]: This is a measure of your ability to generate force. " +
+                "Example: Breaking down doors, lifting heavy objects, etc. \n [Agility]: this is a measure of " +
+                "your ability to move quickly / react to stimuli. Example: Outrunning an enemy, catching a falling " +
+                "item. \n [Perception]: This is a measure of your ability to observe the world around you. Example: " +
+                "Noticing a hidden door. \n [Intelligence]: This is a measure of your knowledge. Example: Knowing " +
+                "how to operate technical machinery. \n [Charisma]: This is a measure of your force of personality. " +
+                "It determines how well you can influence others. Example: Persuading a merchant for a discount. \n " +
+                "You have 5 Ability Points to disperse across these categories, each point is added onto your roll." +
+                " Choose wisely.");
 		
 	}
 
--- DeadOrbit/Program.cs
+++ DeadOrbit/Program.cs
@@ -7,17 +7,36 @@ namespace DeadOrbit
     {
         static void Main(string[] args)
         {
+            // Instantiate GameText Class
             GameText gameText = new GameText();
-
+           
+            //Set up intro to game
             gameText.GameIntro();
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
-
-            gameText.InstructionText("What is your name?");
+            //Getting player name
+            gameText.YellowText("What is your name?");
             string playerName = Console.ReadLine();
 
-            gameText.InstructionText("What is your age?");
-            int playerAge = int.Parse(Console.ReadLine());
+            //Getting player age 
+            int playerAge;
+            bool isValidInput;
+
+            do
+            {
+                gameText.YellowText("What is your age?");
+                string input = Console.ReadLine();
+
+                isValidInput = int.TryParse(input, out playerAge);
+
+                if (!isValidInput)
+                {
+                    gameText.RedText("Invalid selection!");
+                }
+
+
+            } while (!isValidInput);
+            
+            
             bool isOld = false; 
 
             if (playerAge >= 50)
@@ -25,56 +44,116 @@ namespace DeadOrbit
                 isOld = true;
             }
 
-            gameText.InstructionText("Are you male or female? Please enter 'M' or 'F'");
-            string genderInput = Console.ReadLine();
-            bool isMale = true;
-            if (genderInput == "F")
+            //Selecting player gender and verification
+            string genderInput;
+            bool isMale = false;
+
+           do
             {
-                isMale = false;
+                gameText.YellowText("Are you male or female? Please enter 'M' or 'F'");
+                genderInput = Console.ReadLine().ToUpper();
+
+                if (genderInput != "F" && genderInput != "M" )
+                {
+                    gameText.RedText("Invalid Selection!");
+                }
+
+            } while (genderInput != "F" && genderInput != "M");
+
+            if (genderInput == "M") {
+                isMale = true;
             }
 
-            gameText.InstructionText("What is your background? \n [Veteran]: You are a former Space Marine, having " +
+            //Selecting player background and verification
+
+            string backgroundInput; 
+            bool isVeteran = false;
+            bool isSalesman = false;
+            bool isLifter = false;
+
+            do
+            {
+                gameText.YellowText("What is your background? \n [Veteran]: You are a former Space Marine, having " +
                 "served multiple years in the armed forces you are a force to be reckoned with in combat. " +
                 " +2 to Attack Rolls \n [Salesman]: You used to sell manufacturing equipment to colonists many years" +
                 " ago and were quite good at it. You've got a silver tongue. +2 to Charisma Rolls" +
                 "\n [Weightlifter] Once upon a time you were a top tier competitive athlete. While " +
                 "your competition days might be long past you're still stronger than the averagae joe.  +2 to " +
                 "Strength Rolls.\n \n Please enter 'V', 'S', or 'W'");
-            bool isVeteran = false;
-            bool isSalesman = false;
-            bool isLifter = false; 
-            string backgroundInput = Console.ReadLine(); 
-            switch (backgroundInput)
-            {
-                case "V":
-                    isVeteran = true;
-                    break;
-                case "S":
-                    isSalesman = true;
-                    break;
-                case "W":
-                    isLifter = true;
-                    break;
-                default:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("INVALID SELECTION.");
-                    Console.ResetColor();
-                    break;
-            }
 
-            gameText.InstructionText("Dead Orbit is a dice based roleplaying game, meaning that whether or not you " +
-                "succeed in an action is determined by the roll of a D20 (a twenty sided die), adding on " +
-                "your ability modifiers. There are 5 Ability Modifiers: Strength, Agility, Perception " +
-                "Intelligence, and Charisma. \n [Strength]: This is a measure of your ability to generate force. " +
-                "Example: Breaking down doors, lifting heavy objects, etc. \n [Agility]: this is a measure of " +
-                "your ability to move quickly / react to stimuli. Example: Outrunning an enemy, catching a falling " +
-                "item. \n [Perception]: This is a measure of your ability to observe the world around you. Example: " +
-                "Noticing a hidden door. \n [Intelligence]: This is a measure of your knowledge. Example: Knowing " +
-                "how to operate technical machinery. \n [Charisma]: This is a measure of your force of personality. " +
-                "It determines how well you can influence others. Example: Persuading a merchant for a discount. \n " +
-                "You have 5 Ability Points to disperse across these categories, each point is added onto your roll." +
-                " Choose wisely.");
-           
+                backgroundInput = Console.ReadLine().ToUpper();
+                switch (backgroundInput)
+                {
+                    case "V":
+                        isVeteran = true;
+                        break;
+                    case "S":
+                        isSalesman = true;
+                        break;
+                    case "W":
+                        isLifter = true;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid Selection!");
+                        Console.ResetColor();
+                        break;
+                }
+
+            } while (backgroundInput != "V" && backgroundInput != "S" && backgroundInput != "W");
+
+
+            // Explaining Ability Checks and distributing points
+            int STR, AGL, PER, INTEL, CHAR;
+            bool isInputValid;
+            int totalPoints;
+
+            do
+            {
+                totalPoints = 0;
+                gameText.AbilityCheckInfo();
+
+                gameText.YellowText("\n\nSelect your Strength score:");
+                isInputValid = int.TryParse(Console.ReadLine(), out STR);
+                totalPoints += isInputValid ? STR : 0;
+
+                gameText.YellowText("\nSelect your Agility score:");
+                isInputValid = int.TryParse(Console.ReadLine(), out AGL);
+                totalPoints += isInputValid ? AGL : 0;
+
+                gameText.YellowText("\nSelect your Perception score:");
+                isInputValid = int.TryParse(Console.ReadLine(), out PER);
+                totalPoints += isInputValid ? PER : 0;
+
+                gameText.YellowText("\nSelect your Intelligence score:");
+                isInputValid = int.TryParse(Console.ReadLine(), out INTEL);
+                totalPoints += isInputValid ? INTEL : 0;
+
+                gameText.YellowText("\nSelect your Charisma score:");
+                isInputValid = int.TryParse(Console.ReadLine(), out CHAR);
+                totalPoints += isInputValid ? CHAR : 0;
+
+                if (!isInputValid)
+                {
+                    gameText.RedText("Invalid input! Please enter numeric values.");
+                    STR = AGL = PER = INTEL = CHAR = 0;
+                }
+                else if (totalPoints > 5)
+                {
+                    gameText.RedText("Nice try, cheater! You only get 5 points to distribute, or less if you want" +
+                        " to make the game harder on yourself. BUT NO MORE THAN 5! Try AGAIN! \n ");
+                    STR = AGL = PER = INTEL = CHAR = 0;
+                }
+
+            } while (totalPoints > 5 || !isInputValid);
+
+            Console.WriteLine(STR);
+            Console.WriteLine(AGL);
+            Console.WriteLine(PER);
+            Console.WriteLine(INTEL);
+            Console.WriteLine(CHAR);
+
+
 
 
             // Assuming Character is a class you have defined elsewhere
